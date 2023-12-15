a35df6aaa5c200f76d0743ca232ea41f75dd0d9f
Vinay <vinaywebguy@gmail.com>
2023-12-15 17:54:17 +0530

    Project Initialized

---
+++ assets/app.js
@@ -0,0 +1,70 @@
+$('.toggle-icon i').on('mouseover', function() {
+    $('.sidebar').addClass('low')
+})
+$('.toggle-icon i').on('mouseleave', function() {
+    $('.sidebar').removeClass('low')
+})
+$('.toggle-user-menu i').on('mouseover', function() {
+    $('.user-menu-links').addClass('low')
+})
+$('.toggle-user-menu i').on('mouseleave', function() {
+    $('.user-menu-links').removeClass('low')
+})
+
+$(".toggle-icon").click(function() {
+    var icon = $(this).find('i');
+    icon.toggleClass('fa-caret-left fa-caret-right');
+    
+    if ($(this).hasClass('closed')) {
+        $("html").css("--sidebar-width", "250px");
+    } else {
+        $("html").css("--sidebar-width", "0px");
+    }
+    $(this).toggleClass('closed');
+});
+$(".theme-icon").click(function() {
+    $(this).find('i').toggleClass('fa-sun fa-moon');
+
+    if ($("html").css("--clr3") === "#fff") {
+        $("html").css("--clr3", "#222 ");
+        $("html").css("--clr4", "#fff ");
+    } else {
+        $("html").css("--clr3", "#fff");
+        $("html").css("--clr4", "#222");
+    }
+});
+
+$(document).ready(function() {
+    $('.haveSubmenu').click(function(event) {
+        event.preventDefault();
+        var submenu = $(this).next('.sidebar-submenu');
+        if (submenu.height() === 0) {
+            submenu.css('height', 'auto');
+            var autoHeight = submenu.height();
+            submenu.height(0); 
+            submenu.animate({ height: autoHeight }, 150);
+        } else {
+            submenu.animate({ height: '0' }, 150);
+        }
+    });
+    $('.counter-block span').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).data('counter')
+        }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
+
+$(".toggle-user-menu").click(function() {
+    $(this).find('i').toggleClass('fa-caret-left fa-caret-right');
+        $('.user-menu').toggleClass('active');
+});
+
+
+
+
